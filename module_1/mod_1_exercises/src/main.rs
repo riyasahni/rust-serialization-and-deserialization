8dee6d1b3a0f90d9d35af6cbcbd9fce889d7cb0b
riyasahni <riyasahni@gmail.com>
2022-01-13 19:13:18 -0600

    completed module 1!!

--- module_1/mod_1_exercises/src/main.rs
+++ module_1/mod_1_exercises/src/main.rs
@@ -6,4 +6,7 @@ fn main() {
     // You can use main to write tests for your solutions
     // nothing written in main will be used for grading: 
     // make sure to put it all in solution.rs!
+    
+    is_leap_year(2022);
+    hello();
 }
--- module_1/mod_1_exercises/src/solution.rs
+++ module_1/mod_1_exercises/src/solution.rs
@@ -1,7 +1,11 @@
 pub fn hello() -> &'static str {
-    ""
+    "Hello World!"
 }
 
 pub fn is_leap_year(yr: i32) -> bool {
-    false
+    if {yr % 4 == 0 && yr % 100 != 0} || yr % 400 == 0 {
+        return true
+    } else {
+        return false
+    }
 }
