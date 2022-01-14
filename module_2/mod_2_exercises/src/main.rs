84c351f5a327d5ec709868b14b20b44f2e173f24
riyasahni <riyasahni@gmail.com>
2022-01-14 12:16:01 -0600

    adding main and solution for mod 1

--- module_2/mod_2_exercises/src/main.rs
+++ module_2/mod_2_exercises/src/main.rs
@@ -1,3 +1,6 @@
 use solution::{plus_one, Rectangle, Coin, coin_value};
 
-fn main() {}
+fn main() {
+    let mut tester = 5;
+    plus_one(&mut tester);
+}
--- module_2/mod_2_exercises/src/solution.rs
+++ module_2/mod_2_exercises/src/solution.rs
@@ -7,8 +7,9 @@ Step 1: Write a function that increments a number by 1.
         outside of the scope of ownerhsip.
 */
 
-pub fn plus_one(n: &mut i32) {}
-
+pub fn plus_one(n: &mut i32) {
+    *n = *n+1
+}
 
 /*
 TASK 2
@@ -20,12 +21,23 @@ Create a Rectangle struct. Then, implement the following functions:
     2. Calculates the area of the Rectangle.
 */
 
-pub struct Rectangle {}
+pub struct Rectangle {
+    pub width: u8,
+    pub height: u8,
+}
 
 impl Rectangle {
-    pub fn is_square(&self) -> bool {}
+    pub fn is_square(&self) -> bool {
+        if self.width == self.height {
+            return true
+        } else {
+            return false
+        }
+    }
 
-    pub fn calc_area(&self) -> u8 {}
+    pub fn calc_area(&self) -> u8 {
+        self.width * self.height
+    }
 }
 
 /*
@@ -35,8 +47,18 @@ Match - Create a function with a match pattern that is able to sort coins.
 
 */
 
-pub enum Coin {}
+pub enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
 
 pub fn coin_value(coin: Coin) -> u8 {
-    0
+    match coin {
+        Coin::Penny => return 1,
+        Coin::Nickel => return 5,
+        Coin::Dime => return 10,
+        Coin::Quarter => return 25
+    }
 }
